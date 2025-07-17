a6670dc6e8a23e951d91a206ef2fb8dead789199
Anirudh181013 <anirudhbvarma@gmail.com>
2025-07-17 12:23:29 +0200

    changed axiosObject backend url

--- apiClient/axiosObject.js
+++ apiClient/axiosObject.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BACKEND = process.env.BACKEND_URL || "http://localhost:3113/InventorySummary";
+const BACKEND = process.env.BACKEND_URL || "http://localhost:3113/";
 
 const apiClient = axios.create({
     baseURL: BACKEND,
