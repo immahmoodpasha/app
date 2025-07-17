a0bbc2e3dd2e9c46a46113bfc65b583579452793
jasw456 <jaswanthsathyavarapu987@gmail.com>
2025-07-17 09:47:28 +0200

    NewProduct

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './Login';
 import Inventory from './Inventory';
+import NewProduct from './NewProduct';
+import "./App.css";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/inventory" element={<Inventory />} />
-      </Routes>
-    </Router>
+    <div className='App'>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/inventory" element={<Inventory />} />
+          <Route path="/newproduct" element={<NewProduct />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
