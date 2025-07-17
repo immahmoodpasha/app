0e9b9c26e7088c40715ba5bd6c912b21d7e63b62
jasw456 <jaswanthsathyavarapu987@gmail.com>
2025-07-17 13:05:23 +0200

    newproduct form completionand also small change in login styling input field

--- src/NewProduct.js
+++ src/NewProduct.js
@@ -3,14 +3,37 @@ import "./NewProduct.css";
 
 function NewProduct () {
   return (
-    <div>
-        <h2>ADD PRODUCT</h2>
-        <form>
-            <label htmlFor="pn">Product Name:</label>
-            <input type="text" id="pn"/>
-            <input type="" />
-            <button>ADD</button>
+    <div id="addprod-page">
+      <div id="addprod-cont">
+        <h2 id="head-addprod">ADD PRODUCT</h2>
+        <form id="addprod-form">
+          <div id="prod-name">
+            <label htmlFor="pn">Product Name</label>
+            <input type="text" id="pn" required/>
+          </div>
+          <div id="prod-img">
+            <label htmlFor="pn">Product Image URL</label>
+            <input type="text" id="pi" required/>
+          </div>
+          <div id="categ">
+            <label htmlFor="ct">Category</label>
+            <input type="text" id="ct" required/>
+          </div>
+          <div id="quant">
+            <label htmlFor="qn">Quantity</label>
+            <input type="number" id="qn" required/>
+          </div>
+          <div id="un-pr">
+            <label htmlFor="up">Unit Price</label>
+            <input type="number" id="up" required/>
+          </div>
+          <div id="thres">
+            <label htmlFor="th">Threshold</label>
+            <input type="number" id="th" required/>
+          </div>
+          <button id="btn-addprod">ADD</button>
         </form>
+    </div>
     </div>
   )
 }
