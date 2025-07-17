b9530ccbce3c9eed9082b0adbff3ad3d8747f363
Anirudh181013 <anirudhbvarma@gmail.com>
2025-07-17 12:20:58 +0200

    apiClient created

---
+++ apiClient/axiosObject.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const BACKEND = process.env.BACKEND_URL || "http://localhost:3113/InventorySummary";
+
+const apiClient = axios.create({
+    baseURL: BACKEND,
+    headers: {
+        'Content-Type': 'application/json',
+        //Authorization you can put if needed
+    }
+});
+
+export default apiClient;
+
+//import apiClient and then use apiClient.get('{your endpoint path}') for GET request
+//use apiClient.post('{your endpoint path}', data) for POST request
