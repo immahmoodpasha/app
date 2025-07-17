fa279fb343faa4088d0754a9fef854f9fc88b43d
Koushik1403 <mahavadhikoushik@gmail.com>
2025-07-17 14:21:33 +0200

    InventorySummary Completed

--- src/InventorySummary.js
+++ src/InventorySummary.js
@@ -1,5 +1,7 @@
 import {useState, useEffect} from 'react';
 import "./InventorySummary.css";
+import InnventoryIcon from './assets/inventory_icon.png'
+
 function InventorySummary() {
         const url = "http://localhost:3113/InventorySummary"
 
@@ -14,8 +16,11 @@ function InventorySummary() {
         if (!data) return <p>Loading...</p>;
         const {totalItems,changePercentage,lastUpdated,inStock,lowStock,outOfStock} = data
 
+        const inStockNum = Number(data.inStock)
+        const totalItemsNum = Number(data.totalItems)
+
     return (
-    <div class='Inventory-Summary-Card'>
+    <div className='Inventory-Summary-Card'>
         <div className='card-header'>
             <h3 id='Heading1'>Inventory Summary</h3>
             <hr id='header-line'></hr>
@@ -25,11 +30,57 @@ function InventorySummary() {
             <div className='Total-Items'>
                 <h3 id='titems'>{totalItems}</h3>
                 <span><p id='perchange'>↑ {changePercentage}% from last month</p></span>
-                <div id='Inventory-summary-icon1'>
-                    <p>jerkin</p>
+                <p id='icon1'>
+                    <img src={InnventoryIcon} alt="Inventory Icon" style={{height: '30px'}}/>
+                </p>
+            </div>
+            <p className="updated-time">Last updated: Today at {lastUpdated}</p>
+        </div>
+        <div className='progress-bar'>
+                <div className="status-row">
+                    <span>In Stock</span>
+                    <div id='In-Stock'>
+                        <span>{inStock}</span>
+                    </div>
+                </div>
+                <div id='bar-cont-green'>
+                        <div id='bar-green' style={{ width: `${inStockNum/totalItemsNum * 100}%` }}>
+                        </div>
+                </div>
+        </div>
+        
+
+        <div className='progress-bar'>
+                <div className="status-row">
+                <span>Low Stock</span>
+                <div id='low-Stock'>
+                    <span>{lowStock}</span>
                 </div>
             </div>
+            <div id='bar-cont-orange'>
+                    <div id='bar-orange' style={{ width: `${lowStock/totalItemsNum * 100}%` }}>
+                    </div>
+            </div>
+
         </div>
+
+        
+
+        <div className='progress-bar'>
+                <div className="status-row">
+                <span>Out of Stock</span>
+                <div id='out-Stock'>
+                    <span>{outOfStock}</span>
+                </div>
+            </div>
+            <div id='bar-cont-red'>
+                    <div id='bar-red' style={{ width: `${outOfStock/totalItemsNum * 100}%` }}>
+                    </div>
+            </div>
+
+        </div>
+
+        
     </div>
 )
     
