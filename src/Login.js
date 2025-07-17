11d986e4b67d43b4fce784dbc54715b4f29403b7
Anirudh181013 <anirudhbvarma@gmail.com>
2025-07-17 09:04:01 +0200

    Login Container Updates

--- src/Login.js
+++ src/Login.js
@@ -1,19 +1,25 @@
 import './Login.css';
 function Login() {
   return (
-    <div className="main">
-        <div> 
-        <form>
-            <h2>Welcome back!!</h2>
+    <div className='main'>
+      <div id='login_cont'>
+        <div id='welcome_back'>Welcome Back</div>
+        <div id='input_form'>
+          <div id='username'>
             <label>Email</label>
-            <input name="email" type="email" />
+            <input name="email" type="email" placeholder='Enter your email'/>
+          </div>
+          <div id='password'>
             <label>Password</label>
-            <input name="password" type="password"/>
-            <button >submit</button>
-            <span>forgot password?</span>
-        </form>
+            <input name="password" type="password" placeholder='Enter your password'/>
+          </div>
         </div>
+        <button id='submit_btn'>Submit</button>
+        <div id='forgot_pwd'><a>Forgot Password?</a></div>
+        <div id='sign_up'>Don't have an account? <a>Sign Up</a></div>
+      </div>
     </div>
+    
   );
 }
 
