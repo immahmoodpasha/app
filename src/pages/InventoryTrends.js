173d2345665e0938f34b7eed6d5b0847edf450ce
Koushik1403 <mahavadhikoushik@gmail.com>
2025-07-20 00:05:00 +0200

    inventorytrends update

--- src/pages/InventoryTrends.js
+++ src/pages/InventoryTrends.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import "../styles/InventoryTrends.css";
+import arrowup from '../assets/arrow-up.png'
+import arrowdown from '../assets/arrow-down.png'
 
 function InventoryTrends() {
 
@@ -19,12 +21,58 @@ function InventoryTrends() {
     return (
         <div className="inventory-trends-container">
             <div className="inventory-trends-header">
-                <h2 id="main-heading">Inventory Trends</h2>
-                <hr id="header-line"></hr>
-                
+               <h2 id="main-heading">Inventory Trends</h2>
+               <p id="subheading">Last 1Y Data</p>
             </div>
-
-        </div>
+               <hr id="header-line"></hr>
+            
+            <div className="header-below">
+                
+                <div className="arrowup">
+                    <img src={arrowup} alt="arrow up" style={{height:'20px'}} />
+                </div>
+                <div className="below-text">
+                    <p id="most-ordered-item">Most ordered Item</p>
+                    <div className="below-text-item">
+                        <p id="most-ordered-item-name">{data.mostOrdered.item}</p> 
+                        <p id="most-ordered-item-quantity">{data.mostOrdered.quantity}</p>
+                    </div>
+                </div>
+                <div className="arrowdown">
+                    <img src={arrowdown} alt="arrow down" style={{height:'20px'}} />
+                </div>
+                <div className="below-text-right">
+                    <p id="most-ordered-item">Most ordered Item</p>
+                    <div className="below-text-item">
+                        <p id="most-ordered-item-name">{data.mostOrdered.item}</p> 
+                        <p id="most-ordered-item-quantity">{data.mostOrdered.quantity}</p>
+                    </div>
+                </div>   
+            </div>
+            
+            <div className="header-below-below">
+                <div className="arrowup">
+                    <img src={arrowup} alt="arrow up" style={{height:'20px'}} />
+                </div>
+                <div className="below-text">
+                    <p id="most-ordered-item">Most ordered Item</p>
+                    <div className="below-text-item">
+                        <p id="most-ordered-item-name">{data.mostOrdered.item}</p> 
+                        <p id="most-ordered-item-quantity">{data.mostOrdered.quantity}</p>
+                    </div>
+                </div>
+                <div className="arrowdown">
+                    <img src={arrowdown} alt="arrow down" style={{height:'20px'}} />
+                </div>
+                <div className="below-text-right">
+                    <p id="most-ordered-item">Most ordered Item</p>
+                    <div className="below-text-item">
+                        <p id="most-ordered-item-name">{data.mostOrdered.item}</p> 
+                        <p id="most-ordered-item-quantity">{data.mostOrdered.quantity}</p>
+                    </div>
+                </div>   
+            </div>
+    </div>
 
     )
 }
