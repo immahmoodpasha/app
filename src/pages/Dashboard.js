82dd7b440941f56d72cf4d4b416c607b721aff53
Anirudh181013 <anirudhbvarma@gmail.com>
2025-07-22 12:08:24 +0200

    added components to dashboard
    adjusted positioning within each component to top left (each component positioning can be set at dashboard.css)

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,18 +1,62 @@
 import { useState } from "react";
 import '../styles/Dashboard.css';
+import Inventory from "./Inventory";
+import Header from "./Header";
+import InventorySummary from "./InventorySummary";
+import InventoryTrends from "./InventoryTrends";
+import CategoryDistribution from "./CategoryDistribution";
+import CategoryRevenue from "./CategoryRevenue";
+import OverviewAnalytics from "./OverviewAnalytics";
 const Dashboard = () => {
-    
+    const [selectedOption, setSelectedOption] = useState('Inv');
+
+
     
     return(
         <div id="dashboard">
             <div id="header">
-                <div id="logo">LOGO</div>
-                <div id="navbar">
-                    <div id="inventory">Inventory</div>
-                    <div id="sales">Sales</div>
-                </div>
-            <div id="profile">Profile</div>
+            <Header />
             </div>
+            <div id="navMenu">
+                <div id="inventoryOption" onClick={()=>setSelectedOption('Inv')}>Inventory</div>
+                <div id="statisticsOption" onClick={()=>setSelectedOption('St')}>Statistics</div>
+            </div>
+            <div id="content">
+                {selectedOption==='Inv' && (
+                    <div id="tab-content">
+                        <div id="inventoryGraphics">
+                            <div id="categoryDistribution">
+                                <CategoryDistribution />
+                            </div>
+                            <div id="inventorySummary">
+                                <InventorySummary />
+                            </div>
+                        </div>
+                        
+                        <div id="inventory">
+                            <Inventory />
+                        </div>
+                        <div id="categoryRevenue">
+                            <CategoryRevenue />
+                        </div>
+                        <div id="inventoryTrends">
+                            <InventoryTrends />
+                        </div>
+                    </div>
+                    
+                )}
+                {selectedOption==='St' && (
+                    <div id="tab-content">
+                        <div id="overviewAnalytics">
+                            <OverviewAnalytics />
+                        </div>
+                        
+                    </div>
+                    
+                )}
+                
+            </div>
+            
 
         </div>
     );
