be133696a513f01f0120b9bde38315feb74de38d
Koushik1403 <91865318+Koushik1403@users.noreply.github.com>
2025-07-25 00:58:24 +0530

    User signin onpress changed from inventoryPage to dashboardPage

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -10,7 +10,7 @@ function Login() {
   const navigate=useNavigate();
     const handleSubmit=(e)=>{
       e.preventDefault();
-      navigate("/Inventory")
+      navigate("/Dashboard")
       apiClient.post('Users',
         JSON.stringify(
           {
